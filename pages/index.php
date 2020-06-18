eaedd4198061128786175c7096b24cefa218c3ed
Alberto Ferrer <alberto@divalia.mx>
2020-06-18 13:03:38 -0500

    Cambios!

--- pages/index.php
+++ pages/index.php
@@ -48,7 +48,7 @@ if(path(1) == 'post') {
     (d.head || d.body).appendChild(s);
     })();
     </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>     
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
   </article>
 <?php } else { ?>
   <?php foreach($posts as $post) { ?>
@@ -66,7 +66,7 @@ if(path(1) == 'post') {
       <?php } ?>
       <p><?= implode('&nbsp;', [
         $truncate(strip_tags($markdown($post['preview'])), 180),
-        $anchor('continue', $link)
+        $anchor('continuar', $link)
       ]); ?></p>
     </article>
   <?php } ?>
