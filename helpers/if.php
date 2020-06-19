2b8e8bf19b9a3e20e0fd8b696ea121ad1acd3fb2
Alberto Ferrer <alberto@divalia.mx>
2020-06-19 13:42:37 -0500

    Merged changes

--- helpers/if.php
+++ helpers/if.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * If 19.06.1 Arcane Helper
- * https://github.com/MEDIA76/arcane-helpers
+ * If 20.06.1 Arcane Helper
+ * MIT https://helpers.arcane.dev
 **/
 
 return function($conditional, $return = null, $format = "\x20%s") {
+  $passed = func_num_args();
+
   if($conditional) {
     if(strpos($format, '%s') === false) {
       $format = "\x20{$format}=\"%s\"";
     }
 
-    return sprintf($format, $return ?? $conditional);
+    return sprintf($format, $passed > 1 ? $return : $conditional);
   }
 };
 
