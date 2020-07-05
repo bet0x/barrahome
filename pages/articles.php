db4d0fb982c22b8aeaac0ddcd32aed03cd144206
Alberto Ferrer <alberto@divalia.mx>
2020-07-05 17:22:15 -0500

    Directus test

---
+++ pages/articles.php
@@ -0,0 +1,12 @@
+<?php
+if(path(1) == 'articles') {
+  var_dump($directus('Articulos'));
+}
+
+?>
+<article>
+  <?= $markdown([
+    '# Proyectos',
+    'Quisque vitae lorem ex. Fusce scelerisque tempor suscipit. Donec tristique turpis in neque dignissim, et tempus nisi dignissim. Maecenas at enim nibh. In hac habitasse platea dictumst. Vestibulum scelerisque iaculis pulvinar. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam mauris quam, malesuada eu ligula sed, sagittis sagittis felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum ultrices pulvinar tortor ut consequat. Nulla dignissim nisl quam, vel blandit felis fringilla vitae. Duis in placerat risus. Nam tempus eu eros et viverra.'
+  ]); ?>
+</article>
